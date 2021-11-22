a02363b2ac9b12d0d9dfcf7f2a541c730908db94
Ruilin Liu <76539124+rliuup@users.noreply.github.com>
2021-11-22 13:52:08 -0800

    Format

--- win/chocolatey.ps1
+++ win/chocolatey.ps1
@@ -1,43 +1,43 @@
 #Requires -RunAsAdministrator
 
 choco upgrade -yr --no-progress `
-eartrumpet `
-7zip `
-cmder `
-dismplusplus `
-ditto `
-duf `
-everything `
-geekuninstaller `
-git `
-imageglass `
-marktext `
-meld `
-musicbee `
-notepadplusplus `
-oh-my-posh `
-peazip `
-poshgit `
-powershell-core `
-powertoys `
-sumatrapdf `
-sysinternals `
-treesizefree `
-vim `
-windirstat `
+  eartrumpet `
+  7zip `
+  cmder `
+  dismplusplus `
+  ditto `
+  duf `
+  everything `
+  geekuninstaller `
+  git `
+  imageglass `
+  marktext `
+  meld `
+  musicbee `
+  notepadplusplus `
+  oh-my-posh `
+  peazip `
+  poshgit `
+  powershell-core `
+  powertoys `
+  sumatrapdf `
+  sysinternals `
+  treesizefree `
+  vim `
+  windirstat `
 
 # Fonts
 choco upgrade -yr --no-progress `
-anonymouspro `
-cascadia-code-nerd-font `
-cascadiacode `
-FiraCode `
-firacodenf `
-hackfont `
-jetbrainsmono `
-jetbrainsmononf `
-nerdfont-hack `
-sourcecodepro `
+  anonymouspro `
+  cascadia-code-nerd-font `
+  cascadiacode `
+  FiraCode `
+  firacodenf `
+  hackfont `
+  jetbrainsmono `
+  jetbrainsmononf `
+  nerdfont-hack `
+  sourcecodepro `
 
 
 
