4cc18f5aed7d33725382af216545773cb4b14f8f
Ruilin Liu <76539124+rliuup@users.noreply.github.com>
2021-11-22 16:14:31 -0800

    Add cmder config

--- win/setup.ps1
+++ win/setup.ps1
@@ -1,15 +1,41 @@
 #Requires -RunAsAdministrator
 
-New-Item -Path $HOME\.gitconfig -ItemType SymbolicLink -Value $PSScriptRoot\dotfiles\.gitconfig -Force
-New-Item -Path $HOME\.wslconfig -ItemType SymbolicLink -Value $PSScriptRoot\dotfiles\.wslconfig -Force
+Function New-SoftLink {
+  param (
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$Source,
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$Target
+  )
+
+  # remove target file if exists
+  try {
+    if (Test-Path $Target) {
+      Remove-Item $Target
+    }
+    
+    New-Item -Path $Target -ItemType SymbolicLink -Value $Source -Force
+  }
+  catch {
+    Write-Host -ForegroundColor Red "Something went wrong..."
+    Write-Host $_
+  }
+}
+
+New-SoftLink -Source $PSScriptRoot\dotfiles\.gitconfig -Target $HOME\.gitconfig
+New-SoftLink -Source $PSScriptRoot\dotfiles\.wslconfig -Target $HOME\.wslconfig
+
 
 if ($null -ne (Get-Command oh-my-posh).Name) {
   New-Item -ItemType Directory -Force -Path $HOME\.pwsh | Out-Null
-  New-Item -Path $HOME\.pwsh\ohmyposhv3.json -ItemType SymbolicLink -Value $PSScriptRoot\oh-my-posh\ohmyposhv3.json -Force
-  New-Item -Path $HOME\.pwsh\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value $PSScriptRoot\oh-my-posh\Microsoft.PowerShell_profile.ps1 -Force
+  New-SoftLink -Source $PSScriptRoot\oh-my-posh\ohmyposhv3.json -Target $HOME\.pwsh\ohmyposhv3.json
+  New-SoftLink -Source $PSScriptRoot\oh-my-posh\Microsoft.PowerShell_profile.ps1 -Target $HOME\.pwsh\Microsoft.PowerShell_profile.ps1 
 
   # $path = [Environment]::GetFolderPath("MyDocuments")+'\WindowsPowerShell'
   # New-Item -Path $path\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value $PSScriptRoot\oh-my-posh\Microsoft.PowerShell_profile.ps1 -Force
 
-  New-Item -Path $PSHOME\Profile.ps1 -ItemType SymbolicLink -Value $PSScriptRoot\oh-my-posh\Microsoft.PowerShell_profile.ps1 -Force
+  New-SoftLink -Source $PSScriptRoot\oh-my-posh\Microsoft.PowerShell_profile.ps1 -Target $PSHOME\Profile.ps1 
+}
+
+if ($null -ne (Get-Command cmder).Name) {
+  $CMDERHOME = (Get-Command cmder).Source | % { Split-Path -Path $_ }
+  New-SoftLink -Source $PSScriptRoot\cmder\ConEmu.xml -Target $CMDERHOME\vendor\conemu-maximus5\ConEmu.xml 
 }
